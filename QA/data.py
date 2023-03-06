b6bb0f361973904d33521e2dfb3452f4fc559b88
JachinLin <670938249@qq.com>
2023-03-06 13:19:06 +0800

    feat: updata

--- QA/data.py
+++ QA/data.py
@@ -2,8 +2,11 @@ from datasets import *
 from tqdm.auto import tqdm
 import pandas as pd
 import requests
+import io
+import PyPDF2
 
 
+disable_caching()
 id = 0
 count = 0
 def is_chinese(string):
@@ -44,19 +47,36 @@ def split_test():
     # t = raw_datasets['test'].filter(lambda example:example['text'].find('\t')>=0 or is_chinese(example['text']))
     # print(t)
     
+
     
 def request_report(example):
     link = example['url']
-    
+    proxies = {
+        'http': '127.0.0.1:7890',
+        'https': '127.0.0.1:7890',
+    }
     try:
-        print(link)
-        x = requests.get(link)
-        if x.status_code == 200:
-            example['report'] = x.text
+        r = requests.get(link)
+        print(r)
+        if r.status_code == 200:
+            global count
+
+            print(url_source.filter(lambda x: x['report'] != ''))
+            
+            f = io.BytesIO(r.content)
+            reader = PyPDF2.PdfReader(f)
+            content = ''
+            for page in reader.pages:
+                content = content + page.extract_text() + '\n'
+            # print(len(reader.pages))
+            # print(contents)
+            example['report'] = content
             return example
+        else:
+            example['report'] = ''
     except:
-        print(link)
-    example['report'] = ''
+        # print('error {}'.format(link))
+        example['report'] = ''
     return example
 
 def get_all_test_full_report():
@@ -67,14 +87,23 @@ def get_all_test_full_report():
     # print(url_source)
     url_source = load_dataset('csv', data_files='data/test_8_2_url.csv')
     url_source = url_source['train'].select(range(10))
-    url_source = url_source.map(request_report)
+    
+    url_source = url_source.map(request_report,num_proc=10)
+    print(url_source)
+    url_source = url_source.filter(lambda x: x['report'] != '')
     print(url_source)
     url_source = url_source.remove_columns('text')
-    url_source.to_csv('report.csv',index=False)
+    url_source.to_csv('report.csv',index=True)
 
     
-
-
+def process_report():
+    reports = pd.read_csv('/home/linzhisheng/esg/QA/data/report.csv')
+    print(reports)
+    reports = reports[reports['report'].apply(lambda x: is_chinese(x) == False)]
+    reports = reports.rename(columns={'report':'text'})
+    print(reports)
+    reports.to_csv('data/report_eng.csv')
 
 # split_test()
-get_all_test_full_report()
+# get_all_test_full_report()
+process_report()
